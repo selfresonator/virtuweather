3761c228d43671cbc84d012539f0f46369a1cbba
selfresonator <selfresonator@gmail.com>
2016-05-31 19:57:39 -0700

    Cleaned up routes and more Html.

--- client/js/appRoutes.js
+++ client/js/appRoutes.js
@@ -1,18 +1,15 @@
 angular.module('appRoutes', [])
 
 .config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        // templateUrl: 'views/home.html',
-        // controller: 'MainController'
-        templateUrl: 'views/currentWeather.html',
-        controller: 'WeatherController'
-      })
-      // .when('/weather', {
-      //   templateUrl: 'views/currentWeather.html',
-      //   controller: 'WeatherController'
-      // });
 
-  }
+    function($routeProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/currentWeather.html',
+                controller: 'WeatherController'
+            });
+
+    }
+
 ]);
