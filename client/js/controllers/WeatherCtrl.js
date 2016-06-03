248d17e3d0427f9ce521189154d72b1bdbb3b7a3
selfresonator <selfresonator@gmail.com>
2016-06-03 11:44:14 -0700

    Formatted all images to fit screen properly

--- client/js/controllers/WeatherCtrl.js
+++ client/js/controllers/WeatherCtrl.js
@@ -17,27 +17,27 @@
             weatherDataService.getCurrent(city)
                 .then(function(data) {
                     $scope.info.data = data.data;
+                    console.log($scope.info.data);
                 })
                 .then(function() {
                     var dataImage = $scope.info.data.weather[0].main;
                     $scope.city = $scope.info.data.name;
                     if (dataImage === 'Clear') {
-                      $scope.img = '../img/oakland-clear.jpg';
+                      $scope.img = '../img/oakland-clear.jpg';//
                     } else if (dataImage === 'Clouds') {
-                      $scope.img = '../img/oakland-cloudy.jpg';
+                      $scope.img = '../img/clouds.jpg';//
                     } else if (dataImage === 'Snow') {
-                      $scope.img = '../img/snow.jpg';
+                      $scope.img = '../img/snow.jpg';//
                     } else if (dataImage === 'Rain') {
-                      // $scope.img = '../img/rain1.jpg';
-                      $scope.img = '../img/rain2.jpg';
+                      $scope.img = '../img/rain3.jpg';//
                     } else if (dataImage === 'Thunderstorm') {
-                      $scope.img = '../img/thunderstorm.jpg';
+                      $scope.img = '../img/thunderstorm.jpg';//
                     } else if (dataImage === 'Drizzle' || 'Mist') {
-                      $scope.img = '/../img/lightrain.jpg';
+                      $scope.img = '../img/lightrain.jpg';//
                     } else if (dataImage === 'Extreme') {
-                      $scope.img = '/../img/extreme.jpg';
-                    } else if (dataImage === 'Atmosphere') {
-                      $scope.img = '/../img/oakland-hazy.jpg';
+                      $scope.img = '../img/extreme.jpg';//
+                    } else if (dataImage === 'Atmosphere' || 'Haze') {
+                      $scope.img = '../img/haze.jpg';//
                     }
                 });
 
