bfbd51159ee8f3fc64440a52917e47b7a0a6744f
selfresonator <selfresonator@gmail.com>
2016-06-03 15:53:05 -0700

    Clean up and ngAudio 'stop()' method still not working

--- client/js/controllers/WeatherCtrl.js
+++ client/js/controllers/WeatherCtrl.js
@@ -17,56 +17,49 @@
 
             weatherDataService.getCurrent(city)
                 .then(function(data) {
+
                     $scope.info.data = data.data;
+
                 })
                 .then(function() {
+
                     var dataImage = $scope.info.data.weather[0].main;
                     $scope.city = $scope.info.data.name;
                     if (dataImage === 'Clear') {
-                      $scope.img = '../img/oakland-clear.jpg';
-                      $scope.aud = '../sounds/clear.mp3';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/oakland-clear.jpg';
+                        $scope.aud = '../sounds/clear.mp3';
+                        ngAudio.play($scope.aud);
                     } else if (dataImage === 'Clouds') {
-                      $scope.img = '../img/clouds.jpg';
-                      $scope.aud = 'sounds/breeze.mp3';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/clouds.jpg';
+                        $scope.aud = 'sounds/breeze.mp3';
+                        ngAudio.play($scope.aud);
                     } else if (dataImage === 'Snow') {
-                      $scope.img = '../img/snow.jpg';
-                      $scope.aud = '';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/snow.jpg';
+                        $scope.aud = '';
+                        ngAudio.play($scope.aud);
                     } else if (dataImage === 'Rain') {
-                      $scope.img = '../img/rain3.jpg';
-                      $scope.aud = '';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/rain3.jpg';
+                        $scope.aud = '';
+                        ngAudio.play($scope.aud);
                     } else if (dataImage === 'Thunderstorm') {
-                      $scope.img = '../img/thunderstorm.jpg';
-                      $scope.aud = '';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/thunderstorm.jpg';
+                        $scope.aud = '';
+                        ngAudio.play($scope.aud);
                     } else if (dataImage === 'Drizzle' || 'Mist') {
-                      $scope.img = '../img/lightrain.jpg';
-                      $scope.aud = '';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/lightrain.jpg';
+                        $scope.aud = '';
+                        ngAudio.play($scope.aud);
                     } else if (dataImage === 'Extreme') {
-                      $scope.img = '../img/extreme.jpg';
-                      $scope.aud = '';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/extreme.jpg';
+                        $scope.aud = '';
+                        ngAudio.play($scope.aud);
                     } else if (dataImage === 'Atmosphere' || 'Haze') {
-                      $scope.img = '../img/haze.jpg';
-                      $scope.aud = '';
-                      // ngAudio.stop();
-                      ngAudio.play($scope.aud);
+                        $scope.img = '../img/haze.jpg';
+                        $scope.aud = '';
+                        ngAudio.play($scope.aud);
                     }
+
                 });
-
-
-
 
         }
 
