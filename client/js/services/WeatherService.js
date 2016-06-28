badac931caa6d7ca34d824ed1192ee83f84c94cc
selfresonator <selfresonator@gmail.com>
2016-06-28 12:33:03 -0700

    Refactored WeatherService to module pattern

--- client/js/services/WeatherService.js
+++ client/js/services/WeatherService.js
@@ -1,19 +1,22 @@
-angular
-    .module('WeatherService', ['WeatherCtrl'])
-    .factory('weatherDataService', weatherDataService);
+(function() {
+    'use strict';
+    angular
+        .module('WeatherService', ['WeatherCtrl'])
+        .factory('weatherDataService', weatherDataService);
 
 
-function weatherDataService($http) {
+    function weatherDataService($http) {
 
-    return {
-        getCurrent: getCurrent
-    };
+        return {
+            getCurrent: getCurrent
+        };
 
-    function getCurrent(city) {
-        return $http({
-          method: 'GET',
-          url: 'http://api.openweathermap.org/data/2.5/weather?APPID=10b6dff0fe8074bc06adae019b23ab06&q={' + city + '}&units=imperial'
-        });
+        function getCurrent(city) {
+            return $http({
+                method: 'GET',
+                url: 'http://api.openweathermap.org/data/2.5/weather?APPID=10b6dff0fe8074bc06adae019b23ab06&q={' + city + '}&units=imperial'
+            });
+        }
+
     }
-
-}
+})();
